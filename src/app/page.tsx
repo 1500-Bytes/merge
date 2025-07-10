7aecad3a26a188b8057862c63111edb1879bf022
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-07-10 22:36:14 +0500

    add insgest and simple worlflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,36 @@
-import { Client } from "@/components/client";
-import { getQueryClient, trpc } from "@/trpc/server";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
+"use client"
 
-export default async function Home() {
-  const queryClient = getQueryClient()
-  void queryClient.prefetchQuery(trpc.hello.queryOptions({
-    text: "Hello, Abdul"
+import { Client } from "@/components/client";
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary, useMutation } from "@tanstack/react-query";
+import { Suspense } from "react";
+import { toast } from "sonner";
+
+export default function Home() {
+  const trpc = useTRPC()
+
+  const invoke = useMutation(trpc.invoke.mutationOptions({
+    onSuccess: () => {
+      toast.success("Event created successfully!");
+    },
+    onError: (error) => {
+      toast.error("An error occurred while creating the event.");
+    },
+    onSettled: () => {
+      toast.info("Event creation completed.");
+    }
   }))
 
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Client />
-      </Suspense>
-    </HydrationBoundary>
+    <div className="max-w-7xl p-7">
+      <Button size={"lg"} disabled={invoke.isPending} onClick={() => invoke.mutate({
+        text: "yousafbhaikhan@10gmail.com"
+      })}>
+        {invoke.isPending ? 'Loading...' : 'Invoke function'}
+      </Button>
+    </div>
+
   );
 }
