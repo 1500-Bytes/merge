3a14725ca4c597b5542c8e11445ef7c628db3452
AbdulRehman-z <yousafbhaikhan10@gmail.com>
2025-07-11 11:33:08 +0500

    integrate gemini model and performed a successfull  ai job

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,13 +1,19 @@
 import { inngest } from "./client";
-
+import { createAgent, gemini } from "@inngest/agent-kit"
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world", name: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "10s");
-    return {
-      message: `Hello ${event.data.email}!`
-    }
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system: "You are an expert programmer who knows almost everything about frontend and specifically nextjs and shadcn. You write readable, maintainable, and efficient code.",
+      model: gemini({ model: "gemini-2.0-flash" })
+    })
+
+    const output = await codeAgent.run(event.data.value)
+    console.log(output)
+
+    return { output }
   }
 );
