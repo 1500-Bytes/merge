bb8891437bd56f223922c6bccaa717bafb23945d
Abdul Rehman-z <yousafbhaikhan10@gmail.com>
2025-08-12 19:57:44 +0500

    feat: implement project view with data prefetching and loading state

--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,3 +1,8 @@
+import { ProjectView } from "@/components/views/project-view";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+
 type PageProps = {
   params: Promise<{
     projectId: string;
@@ -6,8 +11,25 @@ type PageProps = {
 
 async function Page({ params }: PageProps) {
   const { projectId } = await params;
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.messages.getMany.queryOptions({
+      projectId: projectId,
+    }),
+  );
+  // void queryClient.prefetchQuery(
+  //   trpc.projects.getOne.queryOptions({
+  //     projectId: projectId,
+  //   }),
+  // );
 
-  return <div>Project {projectId}</div>;
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<div>Loading project view...</div>}>
+        <ProjectView projectId={projectId} />
+      </Suspense>
+    </HydrationBoundary>
+  );
 }
 
 export default Page;
